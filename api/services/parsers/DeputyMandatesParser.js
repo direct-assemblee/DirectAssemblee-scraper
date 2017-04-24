4927a3a2079df7d36095413f64065ba14b88a2f0
morgane.plat <morgane.plat@backelite.com>
2017-04-25 00:54:50 +0200

    Parsing current mandate starting date

--- api/services/parsers/DeputyMandatesParser.js
+++ api/services/parsers/DeputyMandatesParser.js
@@ -2,6 +2,8 @@
 var Promise = require("bluebird");
 var htmlparser = require('htmlparser2');
 
+const DATE_REGEX = /((0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4})/i;
+
 var currentMandatesParser = function(callback) {
   var mandates = [];
   var parsedItem = {};
@@ -13,6 +15,7 @@ var currentMandatesParser = function(callback) {
   var previousDeputyMandatesRetrieved = false;
   var reallyExpectPreviousDeputiesMandates = false;
 
+  const TAG_CURRENT_MANDATE = "fonctions-an";
   const TAG_OTHERS = "autres";
   const TAG_PAST_DEPUTY_MANDATES = "mandats-an-historique";
   const TAG_PAST_OTHER_GOUV_MISSIONS = "mandats-nationaux-historique";
@@ -20,7 +23,9 @@ var currentMandatesParser = function(callback) {
 
   return new htmlparser.Parser({
     onopentag: function(tagname, attribs) {
-      if (tagname === 'div' && (attribs.id == TAG_OTHERS || attribs.id == TAG_PAST_DEPUTY_MANDATES || attribs.id == TAG_PAST_OTHER_GOUV_MISSIONS || attribs.id == TAG_PAST_INTL_MISSIONS)) {
+      if (tagname === 'div' && attribs.id == TAG_CURRENT_MANDATE) {
+        expectedType = attribs.id;
+      } else if (tagname === 'div' && (attribs.id == TAG_OTHERS || attribs.id == TAG_PAST_DEPUTY_MANDATES || attribs.id == TAG_PAST_OTHER_GOUV_MISSIONS || attribs.id == TAG_PAST_INTL_MISSIONS)) {
         mandates = [];
         expectedType = attribs.id;
         expectMandates = true;
@@ -42,7 +47,16 @@ var currentMandatesParser = function(callback) {
       }
     },
     ontext: function(text) {
-      if (expectedType === TAG_PAST_DEPUTY_MANDATES) {
+      if (expectedType === TAG_CURRENT_MANDATE) {
+        var trimmed = text.trim()
+        if (trimmed) {
+          var startingDateMatched = trimmed.match(DATE_REGEX);
+          if (startingDateMatched) {
+            parsedItem.currentMandateStartDate = startingDateMatched[1];
+            expectedType = null;
+          }
+        }
+      } else if (expectedType === TAG_PAST_DEPUTY_MANDATES) {
         if (text === "Mandat de député") {
           reallyExpectPreviousDeputiesMandates = true;
         }
