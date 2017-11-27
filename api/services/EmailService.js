278784920ecf0be489c684de4af092cdd95b0a95
Morgane Plat <morgane.plat@backelite.com>
2017-11-27 12:11:40 +0100

    Fixing email service

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -1,19 +1,24 @@
-var Mailgun = require('machinepack-mailgun');
+var api_key = 'key-ebbfc9eff23fa37391a2a2ed9684dc0f';
+var DOMAIN = 'sandbox3a4906a7d94046d5b8d768acb8f615f4.mailgun.org';
+
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
 
 module.exports = {
     sendNewThemeEmail: function (subtheme, done) {
         // Send an html email.
         console.log('Sending new email for subtheme : ' + subtheme);
-        Mailgun.sendHtmlEmail({
-            apiKey: 'key-ebbfc9eff23fa37391a2a2ed9684dc0f',
-            domain: 'sandbox3a4906a7d94046d5b8d768acb8f615f4.mailgun.org',
-            toEmail: 'morgane.plat@gmail.com',
-            toName: '',
+
+        var data = {
+            from: 'Theme Helper <themeHelper@directassemblee.fr>',
+            to: 'morgane.plat@gmail.com',
             subject: 'Nouveau thème à gérer',
-            textMessage: 'Cher ami' + ',\nIl faut ajouter ce nouveau sous-thème : \n' + subtheme + '\n  \nSincerely,\nThe Management',
-            htmlMessage: 'Cher ami' + ',<br><br><p>Il faut ajouter ce nouveau sous-thème : <br>' + subtheme +  '</p><br/><span>Sincerely,</span><br/><strong>The Management</strong>',
-            fromEmail: 'themeHelper@directassemblee.fr',
-            fromName: 'ThemeHelper',
+            text: 'Cher ami' + ',\nIl faut ajouter ce nouveau sous-thème : \n' + subtheme + '\n  \nSincerely,\nThe Management'
+        };
+
+        mailgun.messages().send(data, function (error, body) {
+            if (error) {
+                console.log('error sending email : ' + error);
+            }
         });
     }
 };
