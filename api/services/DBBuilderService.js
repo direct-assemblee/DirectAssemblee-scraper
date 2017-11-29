330326a7f395221c2dd94b2699a81086c864f575
Morgane Plat <morgane.plat@backelite.com>
2017-11-29 01:29:43 +0100

    DB port is now 23306

--- api/services/DBBuilderService.js
+++ api/services/DBBuilderService.js
@@ -7,7 +7,7 @@ let client;
 
 const DB_NAME = process.env.DATABASE_NAME || 'directassemblee';
 const DB_HOST = process.env.DATABASE_HOST || 'localhost';
-const DB_PORT = process.env.DATABASE_PORT || 3306;
+const DB_PORT = process.env.DATABASE_PORT || 23306;
 const DB_USER = process.env.DATABASE_USER || 'root';
 const DB_PASSWORD = process.env.DATABASE_PASSWORD || '';
 const DB_ROOT_PASSWORD = process.env.DATABASE_ROOT_PASSWORD || '';
