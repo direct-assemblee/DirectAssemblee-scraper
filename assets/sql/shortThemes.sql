4d2e121e9b5feba4b3b3414fc9e2d613c5a514c8
Morgane Plat <morgane.plat@backelite.com>
2017-12-15 00:36:55 +0100

    Updating type

--- assets/sql/shortThemes.sql
+++ assets/sql/shortThemes.sql
@@ -17,8 +17,8 @@ INSERT IGNORE `shorttheme` (`id`, `fullName`, `shortName`) VALUES
 (1, 'ordonnances prises sur le fondement de la loi sur le renforcement du dialogue social', 'Ordonnances : renforcement du dialogue social'),
 (2, 'fin de la recherche et de l\'exploitation des hydrocarbures', 'Recherche et exploitation des hydrocarbures'),
 (3, '« eau » et « assainissement », compétences optionnelles des communautés de communes', '« eau / assainissement », communautés de communes'),
-(4, 'prorogation de l\'application de la loi sur l\'état d\'urgence', 'maintien de la loi sur l\'état d\'urgence'),
-(5, 'gestion des milieux aquatiques et prévention des inondations', 'milieux aquatiques / prévention des inondations');
+(4, 'prorogation de l\'application de la loi sur l\'état d\'urgence', 'Maintien de la loi sur l\'état d\'urgence'),
+(5, 'gestion des milieux aquatiques et prévention des inondations', 'Milieux aquatiques / prévention des inondations');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
