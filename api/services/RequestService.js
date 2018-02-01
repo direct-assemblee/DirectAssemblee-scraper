5fd877e349eda946cde52fce15f45f030031010b
Morgane Plat <morgane.plat@backelite.com>
2018-02-01 23:54:57 +0100

    Fixing work notifications

--- api/services/RequestService.js
+++ api/services/RequestService.js
@@ -2,7 +2,8 @@ var Request = require('request-promise');
 
 let apiHost = process.env.API_HOST || 'localhost'
 let apiPort = process.env.API_PORT || '1328';
-let apiBaseUrl = 'http://' + apiHost + ':' + apiPort + '/api/';
+let apiVersion = process.env.API_VERSION || '1328';
+let apiBaseUrl = 'http://' + apiHost + ':' + apiPort + '/api/' + apiVersion;
 
 module.exports = {
     sendDeputiesUpdateNotif: function(deputiesIds) {
